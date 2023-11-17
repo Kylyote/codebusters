6a6f56a18bdfa82066400447557bc61212cae8eb
Kyle Jocoy <kjocoy@gmail.com>
2023-11-16 22:48:01 -0800

    changed connection.js to match for deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codebusters"
+);
 
 module.exports = mongoose.connection;
+
+//user: kjocoy and password: yT9RtLiq3fkkJvkN
