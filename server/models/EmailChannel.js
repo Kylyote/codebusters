8c7a990cc43ebc50b53242b1423dfedaca9825dd
Kyle Jocoy <kjocoy@gmail.com>
2023-11-16 21:21:55 -0800

    added models for emails and messages as well as models that are meant to contain the unique IDs of the emails and messages

---
+++ server/models/EmailChannel.js
@@ -0,0 +1,26 @@
+// This is the ES6 way of importing modules. It's equivalent to "const mongoose = require('mongoose');"
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const emailChannelSchema = new mongoose.Schema({
+  user1: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  user2: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  email: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Email",
+      required: true,
+    },
+  ],
+});
+
+// Using "export" is the same as using "module.exports"
+export const EmailChannel = mongoose.model("EmailChannel", emailChannelSchema);
