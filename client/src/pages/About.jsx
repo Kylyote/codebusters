839f85f281003dfa3f6f2063f7e33ccf627aac97
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-17 18:35:35 -0600

    added about page as the landing page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Who We Are</h1>
       <p> Code Buster is a one stop shop for all your web develpment needs.</p>
       <p> Shopping for hardware? Look no futher, everything you need is just a click away</p>
       <p> In the market to brush up on your skills? The best tutors to be found are all right here.</p>
