1fdeea0f44e590fed1820f65c4a4e6795009b18a
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-20 21:48:09 -0600

    added images with links to the services page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -5,35 +5,32 @@ import websiteBuilding from '/images/websiteBuilding.jpeg';
 import resumeBuilding from '/images/resumeBuilder.png';
 
 const Services = () => {
-  return (
-      <div>
-          <h1 style={{textAlign:"center"}}><strong>Services</strong></h1>
+   return (
+       <div style={{marginLeft: '15px'}}>
+           <h1 style={{textAlign:"center", fontSize: '70px'}}><strong>Services Offered</strong></h1>
 
-          <h3>Click on the service you would need:</h3>
-          <div className="row">
-              <div className="col">
-                 <Link to="/search">
-                     <img src={webDevTutoring} alt="Web Development Tutoring" className="img-fluid" />
-                 </Link>
-              </div>
-              <div className="col">
-                 <Link to="/search">
-                     <img src={codeReview} alt="Code Review" className="img-fluid" />
-                 </Link>
-              </div>
-              <div className="col">
-                 <Link to="/search">
-                     <img src={websiteBuilding} alt="Website Building" className="img-fluid" />
-                 </Link>
-              </div>
-              <div className="col">
-                 <Link to="/resume">
-                     <img src={resumeBuilding} alt="Resume Building" className="img-fluid" />
-                 </Link>
-              </div>
-          </div>
-      </div>
-  )
+            <br></br>
+           <h2><strong>Webdevlopment Tutoring</strong></h2>
+           <p>Suck on a project? Don't know where to beging? Stress no more. Help is just a click away!</p>
+           <p><Link to="/search"><img className='glow-on-hover' src={webDevTutoring} alt="Web Development Tutoring" style={{height: '200px', width: '300px'}} /></Link></p>
+           <br></br>
+
+            <h2><strong>Code Review</strong></h2>
+            <p>Got a bug you can't figure out? Or finished with a project and in need of a professional polish?</p>
+           <p><Link to="/search"><img className='glow-on-hover' src={codeReview} alt="Code Review"style={{height: '200px', width: '300px'}} /></Link></p>
+           <br></br>
+
+            <h2><strong>Website Building</strong></h2>
+            <p>Need a website built? Or maybe just a few pages? We can help!</p>
+           <p><Link to="/search"><img className='glow-on-hover' src={websiteBuilding} alt="Website Building" style={{height: '200px', width: '300px'}}/></Link></p>
+
+           <br></br>
+            <h2><strong>Resume Building</strong></h2>
+            <p>Do you need a resume built emphasizing your coding experience? Help is just a few clicks away!</p>
+           <p><Link to="/resume"><img className='glow-on-hover' src={resumeBuilding} alt="Resume Building"style={{height: '200px', width: '300px'}} /></Link></p>
+       </div>
+   )
 }
 
 export default Services
+
