9b5904761180a295857fd1696809c646d8b57724
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-20 22:51:59 -0600

    added images with links to the services page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -5,32 +5,36 @@ import websiteBuilding from '/images/websiteBuilding.jpeg';
 import resumeBuilding from '/images/resumeBuilder.png';
 
 const Services = () => {
-   return (
-       <div style={{marginLeft: '15px'}}>
-           <h1 style={{textAlign:"center", fontSize: '70px'}}><strong>Services Offered</strong></h1>
-
-            <br></br>
-           <h2><strong>Webdevlopment Tutoring</strong></h2>
-           <p>Suck on a project? Don't know where to beging? Stress no more. Help is just a click away!</p>
-           <p><Link to="/search"><img className='glow-on-hover' src={webDevTutoring} alt="Web Development Tutoring" style={{height: '200px', width: '300px'}} /></Link></p>
-           <br></br>
-
-            <h2><strong>Code Review</strong></h2>
-            <p>Got a bug you can't figure out? Or finished with a project and in need of a professional polish?</p>
-           <p><Link to="/search"><img className='glow-on-hover' src={codeReview} alt="Code Review"style={{height: '200px', width: '300px'}} /></Link></p>
-           <br></br>
-
-            <h2><strong>Website Building</strong></h2>
-            <p>Need a website built? Or maybe just a few pages? We can help!</p>
-           <p><Link to="/search"><img className='glow-on-hover' src={websiteBuilding} alt="Website Building" style={{height: '200px', width: '300px'}}/></Link></p>
-
-           <br></br>
-            <h2><strong>Resume Building</strong></h2>
-            <p>Do you need a resume built emphasizing your coding experience? Help is just a few clicks away!</p>
-           <p><Link to="/resume"><img className='glow-on-hover' src={resumeBuilding} alt="Resume Building"style={{height: '200px', width: '300px'}} /></Link></p>
-       </div>
-   )
+  return (
+      <div className="container">
+          <h1 className="text-center" style={{fontSize: '70px'}}><strong>Services Offered</strong></h1>
+          <br></br>
+          <div className="row">
+              <div  style={{backgroundColor:"red"}}className="col-md-6">
+                 <h2><strong>Webdevlopment Tutoring</strong></h2>
+                 <p>Our tutors provide personalized web development tutoring that will guide you through the process of creating a website. Our tutors have extensive experience in various web development languages and frameworks, and they will help you understand and implement best practices in your code. Whether you're a beginner or an experienced developer looking to enhance your skills, our tutoring service is designed to cater to your needs. With our expert guidance, you can overcome any hurdles and enhance your web development skills.</p>
+                 <p><Link to="/search"><img className='glow-on-hover img-fluid' src={webDevTutoring} alt="Web Development Tutoring" /></Link></p>
+              </div>
+              <div style={{backgroundColor:"lightblue"}} className="col-md-6">
+                 <h2><strong>Code Review</strong></h2>
+                 <p>Our code review service is designed to help you identify and rectify any issues in your code. Our review specialist excel in providing professional reviews that ensure your code is clean, efficient, and ready for deployment. Whether you're struggling with a bug or need a professional review of a completed project, our code review specialists can help. Our team of experienced developers will provide detailed feedback and suggestions to improve your code, ensuring that it meets the highest standards of quality and performance.</p>
+                 <p><Link to="/search"><img className='glow-on-hover img-fluid' src={codeReview} alt="Code Review" /></Link></p>
+              </div>
+          </div>
+          <div className="row">
+              <div style={{backgroundColor:"green"}} className="col-md-6">
+                 <h2><strong>Website Building</strong></h2>
+                 <p>Our coders specialize in building websites from scratch or adding new pages to existing sites. Our website building service ensures your website is not only functional but also visually appealing and user-friendly. We use the latest web development technologies and best practices to create websites that are optimized for performance and are ready to attract and retain your target audience. Whether you're a small business looking for an online presence or a large corporation needing a comprehensive website, we can help.</p>
+                 <p><Link to="/search"><img className='glow-on-hover img-fluid' src={websiteBuilding} alt="Website Building" /></Link></p>
+              </div>
+              <div style={{backgroundColor:"orange"}} className="col-md-6">
+                 <h2><strong>Resume Building</strong></h2>
+                 <p>Creating a resume that highlights your coding experience and showcases your skills can be a challenging task. Our resume builder is designed to help you create a resume that stands out and is tailored to your unique skills and experiences. It uses clear, eye-friendly fonts, white space, and an eye-catching resume header to create the best user experience and keep recruiters on the path. We can help you save your professional web developer resume as a PDF, ensuring the layout stays in shape as it flits through the ether. Whether you're a recent graduate or an experienced developer looking to change careers, our resume builder can help you create a resume that will make a great first impression.</p>
+                 <p><Link to="/resume"><img className='glow-on-hover img-fluid' src={resumeBuilding} alt="Resume Building" /></Link></p>
+              </div>
+          </div>
+      </div>
+  )
 }
 
 export default Services
-
