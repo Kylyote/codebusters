c6c61fa3f6b16918a8c56168db3ccc9a57697987
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-11-20 21:17:07 -0800

    adds resume form

--- client/src/components/LoginModal/Form.jsx
+++ client/src/components/LoginModal/Form.jsx
@@ -4,18 +4,18 @@ import Auth from '../../utils/auth';
 import { ADD_USER, LOGIN } from '../../utils/mutations';
 
 function showPassword() {
-    var x = document.getElementById('showPassword')
-    if (x.type === 'password') {
-        x.type = 'text';
+    var element = document.getElementById('showPassword')
+    if (element.type === 'password') {
+        element.type = 'text';
     } else {
-        x.type = 'password'
+        element.type = 'password'
     }
 }
 
 function Form() {
     const [isLogin, setIsLogin] = useState(true);
     const [form, setForm] = useState({email: '', password: ''});
-    const [addUser] = useMutation(ADD_USER);
+    const [addUser, {signuperror}] = useMutation(ADD_USER);
     const [login, { error }] = useMutation(LOGIN)
 
     const handleInputChange = (event) => {
@@ -41,22 +41,27 @@ function Form() {
     const handleSignupSubmit = async (event) => {
         event.preventDefault();
         //handle form submission here. will transfer from signup/login page
-        const mutationResponse = await addUser({
-            variables: {
-                username: form.username,
-                email: form.email,
-                password: form.password,
-                firstName: form.firstName,
-                lastName: form.lastName
-            }
-        });
-        const token = mutationResponse.data.addUser.token;
-        Auth.login(token);
+        try {
+            const mutationResponse = await addUser({
+                variables: {
+                    username: form.username,
+                    email: form.email,
+                    password: form.password,
+                    firstName: form.firstName,
+                    lastName: form.lastName
+                }
+            });
+            const token = mutationResponse.data.addUser.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e)
+        }
     };
 
     
     return (
         <>
+        <div className='overlay'></div>
         <div className='modal-content'>
             {isLogin ? (
                 <>
@@ -85,7 +90,7 @@ function Form() {
                             </div>
                             <div>
                                 <label htmlFor="showPassword">Show Password</label>
-                                <input type="checkbox" onClick={showPassword} />
+                                <input className='show-password' type="checkbox" onClick={showPassword} />
                             </div>
                             {error ? (
                                 <div className='error-text-div'>
@@ -144,19 +149,23 @@ function Form() {
                             <input 
                                 name='password'
                                 type='password'
-                                id='password'
+                                id='showPassword'
                                 placeholder='Password'
                                 onChange={handleInputChange} />
                         </div>
-                        {error ? (
+                        <div>
+                            <label htmlFor="showPassword">Show Password</label>
+                            <input className='show-password' type="checkbox" onClick={showPassword} />
+                        </div>
+                        {signuperror ? (
                                 <div className='error-text-div'>
-                                  <p className="error-text">Please Fill out every field</p>
+                                  <p className="error-text">Please fill out each field</p>
                                 </div>
                             ) : null}
                         <button className='form-submit' type='submit'>Signup</button>
                     </form>
                 </div>
-                <button className='form-change' onClick={() => setIsLogin(true)}>Already have an account? Login here</button>
+                <button className='form-change' onClick={() => setIsLogin(true)}>Already have an account? Login here!</button>
                 </>
             )}
         </div>
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,13 +1,29 @@
-
-
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 const resumeBuilder = () => {
     return (
-      <div>
-     <p>Insert Resume Builder Here</p>
-     <p>Maybe on this page we could use an API for searching local jobs in an area? As a reason to use the Resume Builder (indeed, linked in, glassdoor)</p>
-
-     </div>
+      <>
+      <div className='resumeForm'>
+        <Form>
+          <Form.Group className='mb-3 justify-content-evenly' controlId='formFirstName'>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type='text' placeholder='First Name'/>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type='text' placeholder='Last Name'/>
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type='text' placeholder='Phone Number'/>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email' placeholder='Email'/>
+            <Form.Label>Address</Form.Label>
+            <Form.Control type='text' placeholder='Address'/>
+            <Form.Label>About Me</Form.Label>
+            <Form.Control type='textfield' placeholder='About Me'/>
+          </Form.Group>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+      </>
     )
   };
   
