d26beb0038756e57e6af724c63d69d1cd660415d
sky6565 <godwin1988b@outlook.com>
2023-11-27 19:55:32 -0800

    Profile Page

--- client/src/components/MyForm.jsx
+++ client/src/components/MyForm.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
-// import { socket } from '../socket';
-import io from 'socket.io-client';
-const socket = io('http://localhost:3001');
+import React, { useState } from "react";
+import { socket } from "../socket";
 
 export function MyForm() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [messageReceived, setMessageReceived] = useState("");
 
   function onSubmit(event) {
     event.preventDefault();
     setIsLoading(true);
 
-    socket.timeout(5000).emit('create-something', value, () => {
+    socket.timeout(5000).emit("create-something", value, () => {
       setIsLoading(false);
     });
   }
 
   return (
-    <form onSubmit={ onSubmit }>
-      <input style={{width:"350px", height:"250px", marginLeft:'15px'}} onChange={ e => setValue(e.target.value) } placeholder="Send Message" />
-     
- <br></br>
-
-      <button type="submit" disabled={ isLoading } >Send Message</button>
-     
+    <form onSubmit={onSubmit} style={{ width: "100%" }}>
+      <div
+        id="messages"
+        style={{ minHeight: "50vh", backgroundColor: "white" }}
+      ></div>
+      <div className="form-group row">
+        <label for="exampleFormControlTextarea1">What's on your mind ?</label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="form-group-row">
+        <button type="submit" disabled={isLoading}>
+          Send Message
+        </button>
+      </div>
     </form>
   );
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,29 +1,26 @@
-import profileImage from '../assets/img/Profile.jpeg'
-import { useQuery } from '@apollo/client';
-import { GET_CHAT } from '../utils/queries';
-import {ADD_CHAT_MESSAGE} from '../utils/mutations'
-import {useMutation} from '@apollo/client'
-import {UPDATE_USER} from '../utils/mutations'
-import {QUERY_USER} from '../utils/queries'
-import { useParams } from 'react-router-dom';
-import avatar1 from '../assets/img/avatar_png_files/avatar_1.png';
-import avatar2 from '../assets/img/avatar_png_files/avatar_2.png';
-import avatar3 from '../assets/img/avatar_png_files/avatar_3.png';
-import avatar4 from '../assets/img/avatar_png_files/avatar_4.png';
-import avatar5 from '../assets/img/avatar_png_files/avatar_5.png';
-import avatar6 from '../assets/img/avatar_png_files/avatar_6.png';
-import avatar7 from '../assets/img/avatar_png_files/avatar_7.png';
-import avatar8 from '../assets/img/avatar_png_files/avatar_8.png';
-import avatar9 from '../assets/img/avatar_png_files/avatar_9.png';
-import avatar10 from '../assets/img/avatar_png_files/avatar_10.png';
-import avatar11 from '../assets/img/avatar_png_files/avatar_11.png';
-import avatar12 from '../assets/img/avatar_png_files/avatar_12.png';
-import avatar13 from '../assets/img/avatar_png_files/avatar_13.png';
-import avatar14 from '../assets/img/avatar_png_files/avatar_14.png';
-import ChatModal from '../components/ChatModal';
+import profileImage from "../assets/img/Profile.jpeg";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { UPDATE_USER } from "../utils/mutations";
+import { QUERY_USER } from "../utils/queries";
+import { useParams } from "react-router-dom";
+import avatar1 from "../assets/img/avatar_png_files/avatar_1.png";
+import avatar2 from "../assets/img/avatar_png_files/avatar_2.png";
+import avatar3 from "../assets/img/avatar_png_files/avatar_3.png";
+import avatar4 from "../assets/img/avatar_png_files/avatar_4.png";
+import avatar5 from "../assets/img/avatar_png_files/avatar_5.png";
+import avatar6 from "../assets/img/avatar_png_files/avatar_6.png";
+import avatar7 from "../assets/img/avatar_png_files/avatar_7.png";
+import avatar8 from "../assets/img/avatar_png_files/avatar_8.png";
+import avatar9 from "../assets/img/avatar_png_files/avatar_9.png";
+import avatar10 from "../assets/img/avatar_png_files/avatar_10.png";
+import avatar11 from "../assets/img/avatar_png_files/avatar_11.png";
+import avatar12 from "../assets/img/avatar_png_files/avatar_12.png";
+import avatar13 from "../assets/img/avatar_png_files/avatar_13.png";
+import avatar14 from "../assets/img/avatar_png_files/avatar_14.png";
 
 const avatars = [
- avatar1,
+  avatar1,
   avatar2,
   avatar3,
   avatar4,
@@ -36,170 +33,281 @@ const avatars = [
   avatar11,
   avatar12,
   avatar13,
-  avatar14
- ];
+  avatar14,
+];
 
- const randomAvatar = avatars[Math.floor(Math.random() * avatars.length)];
-
+const randomAvatar = avatars[Math.floor(Math.random() * avatars.length)];
 
 /* Socket.io */
-import {useState, useEffect} from 'react'
-import {socket} from '../socket'
-import {ConnectionState} from '../components/ConnectionState'
-import {ConnectionManager} from '../components/ConnectionManager'
-import {Events} from '../components/Events'
-import {MyForm} from '../components/MyForm'
-import io from 'socket.io-client'
+import { useState, useEffect } from "react";
+import { socket } from "../socket";
+import { ConnectionState } from "../components/ConnectionState";
+import { ConnectionManager } from "../components/ConnectionManager";
+import { Events } from "../components/Events";
+import { MyForm } from "../components/MyForm";
 /* End Socket.IO */
-const onConnect = () => {
-  console.log('connected');
-};
-const onDisconnect = () => {
-  console.log('disconnected');
-};
-const onFooEvent = (data) => {
-  console.log('foo', data);
-}
 
 const Profile = () => {
-  
-  const {id} = useParams();
-  
-    /* Socket.io state variables */
-const [isConnected, setIsConnected] = useState(socket.connected)
-const [fooEvents, setFooEvents] = useState([])
-const [room, setRoom] = useState(id);
-const [showModal, setShowModal] = useState(false);
-{showModal && <ChatModal showModal={showModal} />}
+  const { id } = useParams();
 
+  /* Socket.io state variables */
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
 
-/* socket io */
-const [updateUser] = useMutation(UPDATE_USER);
+  /* socket io */
+  const [updateUser] = useMutation(UPDATE_USER);
 
-const joinRoom = () => {
-  if (room !== '') {
-    socket.emit('join_room', room);
-  }
-};
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
 
-const sendMessage = () => {
-  socket.emit('send_message', message, room);
-};
+    function onDisconnect() {
+      setIsConnected(false);
+    }
 
-    useEffect(() => {
-      socket.on('connect', onConnect);
-      socket.on('disconnect', onDisconnect);
-      socket.on('foo', onFooEvent);
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
 
-      socket.on('receive_message', (data) => {
-        setMessageReceived(data.message);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
+  /* end socket io */
+
+  const { loading, error, data } = useQuery(QUERY_USER, {
+    variables: { id },
+  });
+  console.log(data, "profile line 90");
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  const firstName = data.user.firstName;
+  const lastName = data.user.lastName;
+  const username = data.user.username;
+  const languages = data.user.languages;
+  const email = data.user.email;
+  const skills = data.user.skills;
+
+  /* function for handling page editing */
+
+  function Editable({ text, type, onChange }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(text);
+
+    const handleChange = (e) => {
+      setValue(e.target.value);
+      onChange(e.target.value);
+    };
+
+    const handleEdit = () => {
+      setIsEditing(true);
+    };
+
+    const { loading, error, data } = useQuery(QUERY_USER);
+
+    // ...
+
+    function handleSave() {
+      setIsEditing(false);
+      updateUser({
+        variables: {
+          id: data.user._id,
+          username: username,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        },
       });
-     
-      return () => {
-        socket.off('connect');
-        socket.off('receive_message');
-        socket.off('disconnect');
-        socket.off('foo');
-      };
-     }, [socket]);
-
-     
-  
-
-    /* end socket io */
-
-    const { loading, error, data } = useQuery(QUERY_USER, {
-      variables: {id}});
-   
-
-    if (loading) return 'Loading...';
- if (error) return `Error! ${error.message}`;
-
- const firstName = data.user.firstName;
-    const lastName = data.user.lastName;
-    const username = data.user.username;
-    const languages = data.user.languages;
-    const email = data.user.email;
-    const skills = data.user.skills;
-
-
-    /* function for handling page editing */
-    
-    function Editable({ text, type, onChange }) {
-      const [isEditing, setIsEditing] = useState(false);
-      const [value, setValue] = useState(text);
-      
-      const handleChange = e => {
-        setValue(e.target.value);
-        onChange(e.target.value);
-      };
-      
-      const handleEdit = () => {
-        setIsEditing(true);
-      };
-     
-      const handleSave = () => {
-        setIsEditing(false);
-        updateUser({ variables: { id: data.user._id, username: username, firstName: firstName, lastName: lastName, email: email} });
-      };
-     
-      return (
-        <span onClick={handleEdit}>
-          {isEditing ? (
-            <input type={type} value={value} onChange={handleChange} onBlur={handleSave} />
-          ) : (
-            <span>{value}</span>
-          )}
-        </span>
-      );
-     }
-     
-     
-
-    /* end function for handling page editing */
-
-   
+    }
 
     return (
-      
-        
-        <div>
-          {showModal && <ChatModal showModal={showModal} />}
-            <br></br>
-            <img src={randomAvatar}alt="Profile Picture" className="profile-pic" style={{marginLeft:'15px'}} />
-            <h2 style={{marginLeft:'15px'}}>Name: <Editable text={`${firstName} ${lastName}`} type="text" onChange={(newValue) => { console.log(newValue); }} /> </h2>
+      <span onClick={handleEdit}>
+        {isEditing ? (
+          <input
+            type={type}
+            value={value}
+            onChange={handleChange}
+            onBlur={handleSave}
+          />
+        ) : (
+          <span>{value}</span>
+        )}
+      </span>
+    );
+  }
 
-            <p style={{marginLeft:'15px'}}><strong>Username:</strong><Editable text={`${username}`} type="text" onChange={(newValue) => { console.log(newValue); }} /></p>
-            {/* <p style={{marginLeft:'15px'}}><strong>Email: </strong><Editable text={`${email}`} type="text" onChange={(newValue) => { console.log(newValue); }} /></p> */}
-            {/* <p style={{marginLeft:'15px'}}><strong>Skill: <Editable text={`${languages}`} type="text" onChange={(newValue) => { console.log(newValue); }} /></strong> </p> */}
-            {languages && languages.map((language, index) => (
-  <p style={{marginLeft:'15px'}} key={index}><strong>{language.language}</strong> - {language.skill}</p>
-))}
-            <div className="projects" style={{marginLeft:'15px'}}>
-                <a href="https://github.com/user/project1">Project 1</a>
-                <a href="https://github.com/user/project2">Project 2</a>
-                <a href="https://github.com/user/project3">Project 3</a>
-            </div>
-         
-<div>
-{/* <ChatModal /> */}
-<button style={{marginLeft:'15px'}} onClick={() => {
- window.location.href = `mailto:${email}?subject=CodeBuster%20Support&body=Hello%20${firstName} ${lastName}%20I%20would%20like%20to%20discuss%20using%20you%20for%20services%20I%20need.`;
- setShowModal(!showModal);
- console.log(showModal);
-}}>
- Contact User
-</button>
+  /* end function for handling page editing */
 
-</div>
+  return (
+    <div className="container row">
+      <div className="main-section col-8 row">
+        <div className="col-4">
+          <br></br>
+          <img
+            src={randomAvatar}
+            alt="Profile Picture"
+            className="profile-pic"
+            style={{ marginLeft: "1px " }}
+          />
         </div>
-        
-        
-    )
- }
- 
- export default Profile
+        <div className="col-8">
+          <br></br>
+          <h2 style={{ marginLeft: "15px" }}>
+            Name:{" "}
+            <Editable
+              text={`${firstName} ${lastName}`}
+              type="text"
+              onChange={(newValue) => {
+                console.log(newValue);
+              }}
+            />{" "}
+          </h2>
 
+          <p style={{ marginLeft: "15px" }}>
+            <strong>Username:</strong>
+            <Editable
+              text={`${username}`}
+              type="text"
+              onChange={(newValue) => {
+                console.log(newValue);
+              }}
+            />
+          </p>
+          <p style={{ marginLeft: "15px" }}>
+            <strong>Email: </strong>
+            <Editable
+              text={`${email}`}
+              type="text"
+              onChange={(newValue) => {
+                console.log(newValue);
+              }}
+            />
+          </p>
+          <p style={{ marginLeft: "15px" }}>
+            <strong>
+              Skill:{" "}
+              <Editable
+                text={`${languages}`}
+                type="text"
+                onChange={(newValue) => {
+                  console.log(newValue);
+                }}
+              />
+            </strong>{" "}
+          </p>
 
+          {languages &&
+            languages.map((language, index) => (
+              <p key={index}>
+                <strong>{language.language}</strong> - {language.skill}
+              </p>
+            ))}
+        </div>
+        <div className="container row">
+          <div
+            className="col-3"
+            style={{
+              minHeight: "200px",
+              minWidth: "100px",
+              margin: "55px 5px 5px 5px",
+              border: "35px 35px 35px 35px",
+              padding: "1px 1px 1px 1px",
+              boxShadow: "1px 1px 1px 1px",
+            }}
+          >
+            <br></br>
+            <a href="https://github.com/user/project1">Project 1</a>
+            {/* <iframe
+            src="https://tailwindcss.com/"
+            height="200"
+            width="250"
+            title=""
+          ></iframe> */}
+          </div>
 
+          <div
+            className="col-3"
+            style={{
+              minHeight: "200px",
+              minWidth: "100px",
+              margin: "55px 5px 5px 5px",
+              border: "35px 35px 35px 35px",
+              padding: "1px 1px 1px 1px",
+              boxShadow: "1px 1px 1px 1px",
+            }}
+          >
+            <br></br>
 
+            <a href="https://github.com/user/project2">Project 2</a>
+          </div>
+          <div
+            className="col-3"
+            style={{
+              minHeight: "200px",
+              minWidth: "100px",
+              margin: "55px 5px 5px 5px",
+              border: "35px 35px 35px 35px",
+              padding: "1px 1px 1px 1px",
+              boxShadow: "1px 1px 1px 1px",
+              clear: "both",
+            }}
+          >
+            <br></br>
+            <a href="https://github.com/user/project3">Project 3</a>
+          </div>
+        </div>
+      </div>
+
+      {/* <section class="bg-light">
+        <div class="container">
+          <div class="text-center">
+            <h1>
+              Messenger <ConnectionManager />
+            </h1>
+          </div>
+        </div>
+      </section> */}
+      <div
+        className="col-4"
+        style={{
+          // float: "auto",
+          // position: "fixed",
+          clear: "both",
+          bottom: "0",
+          border: "2px solid black",
+          // marginRight: "20px",
+          // marginBottom: "10px",
+          // width: "400px",
+          // height: "400px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "lightblue",
+            color: "white",
+            padding: "2px",
+          }}
+        >
+          <h1>
+            Messenger <ConnectionManager />
+          </h1>
+        </div>
+        <ConnectionState isConnected={isConnected} />
+        <Events events={fooEvents} />
+        <MyForm />
+      </div>
+    </div>
+  );
+  console.log(data);
+};
+
+export default Profile;
