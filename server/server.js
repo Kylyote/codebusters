4e3b438e2c31dee67dbf7256deeb3be6cb18d75a
Kyle Jocoy <kjocoy@gmail.com>
2023-11-28 23:35:23 -0800

    commenting out code that is not being used to try and fix Heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
-const { authMiddleware } = require('./utils/auth');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,31 +17,28 @@ const server = new ApolloServer({
 });
 
 /* Socket.io */
-const http = require('http')
-const {Server} = require('socket.io')
-const httpServer = http.createServer(app)
-const io = new Server(httpServer
-, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
-  }
-});
+const http = require("http");
+// const { Server } = require("socket.io");
+const httpServer = http.createServer(app);
+// const io = new Server(httpServer
+// , {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST']
+//   }
+// });
 
-io.on('connection', (socket) => {
-  console.log(`a user connected:${socket.id}`) 
-  socket.on("join_room", (data) => {
-    socket.join(data)
-    console.log(`User with ID: ${socket.id} joined room: ${data}`)
-  })
-  socket.on("send_message", (data) => {
-    console.log(data)
-    socket.to(data.room).emit("receive_message", data)
-  })
- });
- 
-
-
+// io.on("connection", (socket) => {
+//   console.log(`a user connected:${socket.id}`);
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+//   socket.on("send_message", (data) => {
+//     console.log(data);
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
@@ -51,31 +48,35 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   // Serve up static assets
-  app.use('/images', express.static(path.join(__dirname, '../client/images')));
+  app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
-  app.use(cors({
-    origin: 'http://localhost:3000',
-   methods: ['GET', 'POST'],
-   allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  // app.use(cors({
+  //   origin: 'http://localhost:3000',
+  //  methods: ['GET', 'POST'],
+  //  allowedHeaders: ['Content-Type', 'Authorization']
+  // }));
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
-    httpServer.listen(PORT, () => { /*changed app.listen to httpServer.listen*/
+  db.once("open", () => {
+    httpServer.listen(PORT, () => {
+      /*changed app.listen to httpServer.listen*/
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-      console.log(`Socket.io server running on port ${PORT}`)
+      // console.log(`Socket.io server running on port ${PORT}`);
     });
   });
 };
